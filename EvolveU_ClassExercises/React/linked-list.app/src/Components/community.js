aedaa7cb2782a61bd263387d29465061923f83e0
ZacNovak <zac.w.novak@gmail.com>
2018-11-22 11:32:28 -0700

    Nov. 22 push

--- EvolveU_ClassExercises/React/linked-list.app/src/Components/community.js
+++ EvolveU_ClassExercises/React/linked-list.app/src/Components/community.js
@@ -1,19 +1,19 @@
- class People {
-    constructor(name, age, dollars){
-        this.name = name;
-        this.age = age;
-        this.dollars = dollars;
-    }
+//  class People {
+//     constructor(name, age, dollars){
+//         this.name = name;
+//         this.age = age;
+//         this.dollars = dollars;
+//     }
 
-    show(person){
-    	return `Name: ${person.name} - Age: ${person.age} - Dollars: ${person.dollar}`   
-    }
+//     show(person){
+//     	return `Name: ${person.name} - Age: ${person.age} - Dollars: ${person.dollar}`   
+//     }
 
 
-    birthday(){
-    	return 0;
-    } 
+//     birthday(){
+//     	return 0;
+//     } 
 
-}
+// }
     
 
--- EvolveU_ClassExercises/React/linked-list.app/src/Components/community.test.js
+++ EvolveU_ClassExercises/React/linked-list.app/src/Components/community.test.js
@@ -1,11 +1,11 @@
-import c from "./community";
+// import c from "./community";
 
-test('Lets test', () => {
-	console.log("Hello World from community!");
-});
+// test('Lets test', () => {
+// 	console.log("Hello World from community!");
+// });
 
-test('lets test', () => {
-    let x = new c.People('Zac', 29, 10.00);
-    expect(x.show()).toBe('Name: Zac - Age: 29 - Dollars: 10.00');
+// test('lets test', () => {
+//     let x = new c.People('Zac', 29, 10.00);
+//     expect(x.show()).toBe('Name: Zac - Age: 29 - Dollars: 10.00');
 
-}
+// }
